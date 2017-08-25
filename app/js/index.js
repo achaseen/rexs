9a9e859d5a1a317e6f2584c6355186dc9dd8ff3e
isembukutti <imasha@imashas-mbp.attlocal.net>
2017-08-25 14:09:28 -0700

    first prototype

--- app/js/index.js
+++ app/js/index.js
@@ -3,6 +3,23 @@ $(window).scroll(function() {
         $("#header").addClass("highlight");
     }
     else {
-      $("#header").removeClass("highlight")
+      $("#header").removeClass("highlight");
     }
 });
+
+$(document).ready(function(){
+  $("#menu-icon").click(function() {
+    $(".sidenav").css("width", "250px");
+    $("#page").css("margin-left", "250px");
+    $("#header").css("left", "250px");
+    $("body").css("background-color", "rgba(0,0,0,0.4)"); 
+  });
+
+  $(".sidenav a").click(function() {
+    $(".sidenav").css("width", "0");
+    $("#page").css("margin-left", "0");
+    $("#header").css("left", "0");
+    $("body").css("background-color", "transparent");
+  });
+});
+
