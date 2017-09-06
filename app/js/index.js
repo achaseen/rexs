50c21686ca52939691cbd4f966b50858b35cbdc0
isembukutti <imasha@imashas-mbp.attlocal.net>
2017-09-05 21:58:10 -0700

    cover images

--- app/js/index.js
+++ app/js/index.js
@@ -1,9 +1,11 @@
 $(window).scroll(function() {
-    if ($(this).scrollTop() > $("#header").height()) {
+    if ($(this).scrollTop() > 400) {
         $("#header").addClass("highlight");
+        //$("#cover-bg-wrapper").css("margin-top", $("#header").height());
     }
     else {
       $("#header").removeClass("highlight");
+      //$("#cover-bg-wrapper").css("margin-top", 0);
     }
 });
 
@@ -12,14 +14,26 @@ $(document).ready(function(){
     $(".sidenav").css("width", "250px");
     $("#page").css("margin-left", "250px");
     $("#header").css("left", "250px");
-    $("body").css("background-color", "rgba(0,0,0,0.4)"); 
+    $("body, #header-content, #header-bg").css("background-color", "rgba(0,0,0,0.4)"); 
   });
 
   $(".sidenav a").click(function() {
     $(".sidenav").css("width", "0");
     $("#page").css("margin-left", "0");
     $("#header").css("left", "0");
-    $("body").css("background-color", "transparent");
+    $("body, #header-content, #header-bg").css("background-color", "transparent");
   });
+
+  $(".footer-links a, .sidenav a, .topnav a").click(function(evt){
+    var hash = $(evt.target).prop("hash");
+
+    $(".topnav a, .sidenav a").removeClass("active");
+    $(".topnav a[href='" + hash + "'], .sidenav a[href='" + hash + "']").addClass("active");
+
+    $('html, body').animate({
+      scrollTop: 0
+    }, 0);
+  }) 
+
 });
 
