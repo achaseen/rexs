465916df1576864f471e1a4c3b04e2a9388fe165
isembukutti <imasha@imashas-mbp.attlocal.net>
2017-09-07 00:45:40 -0700

    fixed side nav

--- app/js/index.js
+++ app/js/index.js
@@ -12,16 +12,12 @@ $(window).scroll(function() {
 $(document).ready(function(){
   $("#menu-icon").click(function() {
     $(".sidenav").css("width", "250px");
-    $("#page").css("margin-left", "250px");
-    $("#header").css("left", "250px");
-    $("body, #header-content, #header-bg").css("background-color", "rgba(0,0,0,0.4)"); 
+    $("#overlay").show();
   });
 
   $(".sidenav a").click(function() {
     $(".sidenav").css("width", "0");
-    $("#page").css("margin-left", "0");
-    $("#header").css("left", "0");
-    $("body, #header-content, #header-bg").css("background-color", "transparent");
+    $("#overlay").hide();
   });
 
   $(".footer-links a, .sidenav a, .topnav a").click(function(evt){
